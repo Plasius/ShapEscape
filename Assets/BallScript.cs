ddf0cc606433974a5725892ec9a00f7f93c80eab
Plasius <Plasius@users.noreply.github.com>
2016-11-15 18:55:18 +0200

    Let's get it started!
    This is what we have so far. A simple click and drag mechanic.

---
+++ Assets/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BallScript : MonoBehaviour {
+
+	float x;
+	float y;
+	bool started=false;
+
+	// Update is called once per frame
+	void Update(){
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+	}
+
+	void OnMouseDrag(){
+		if (!started)
+			started = true;
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Destroy (this.gameObject);
+		SceneManager.LoadScene ("GameScene");
+
+	}
+
+
+	void LateUpdate(){
+		if(started)
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
+	}
+
+}
